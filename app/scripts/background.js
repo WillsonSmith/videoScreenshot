155f0db36e6bfbf0821d9cdd859675871b17b324
Willson Smith <willsonsmth@gmail.com>
2014-04-25 15:51:42 -0400

    initial fix for cross-origin. Also adds fixes for resized video content

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -10,9 +10,41 @@ chrome.contextMenus.create({
         "onclick": function(e) {
           console.log(e);
           var url = e.srcUrl.match(/[^/]+$/);
+          var fullUrl = e.srcUrl;
+
+          var exploded = e.srcUrl.split("/");
+
+          var baseUrl = exploded[0] + "//" + exploded[1] + exploded[2] + "/";
+
           chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
 
-            chrome.tabs.sendMessage(tabs[0].id, {action: "create_image", src: url}, function(response) {});
+            /*chrome.permissions.request({
+              origins: [baseUrl + "*"]
+            });*/
+
+            chrome.tabs.sendMessage(tabs[0].id, {action: "create_image", src: url, full: fullUrl, base: baseUrl}, function(response) {});
+
+            /*var vid = document.createElement('iframe');
+
+            var div = document.createElement('div');
+
+            var iframeBody;
+            var date = new Date();
+            var time = date.getTime();
+              vid.width = 600;
+              vid.height = 500;
+              console.log(fullUrl);
+              vid.src = fullUrl;
+              vid.id = time;
+              //console.log(vid.contentWindow.documen);
+
+              document.getElementsByTagName('body')[0].appendChild(vid);
+
+              document.getElementById(time).onload = function(){
+                iframeBody = document.getElementById(time).contentWindow.document.querySelector('body');
+                console.log(document.getElementById(time).contentWindow.document.querySelector('body').innerHTML);
+                iframeBody.appendChild(div);
+              }*/
           });
         }
       });
--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,3 +1,11 @@
+/*To Do
+  - Hide iframe
+  - Pause video
+  - Get current paused time (top window)
+  - set video in iFrame to paused
+  - THEN take screenshot
+  */
+
 var ext = (function extension(){
 
   var videosOnPage,
@@ -7,53 +15,160 @@ var ext = (function extension(){
 
     init: function(){
 
+      var exploded = document.URL.split("/");
+
+      var baseUrl = exploded[0] + "//" + exploded[1] + exploded[2] + "/";
+
+        ext.createCanvas();
+
+        ext.checkTop();
+
+        ext.msgHandler(baseUrl);
+
+
+    },
+
+    createCanvas: function(){
+
       var body = document.getElementsByTagName('body')[0],
           can = document.createElement('canvas'),
           date = new Date();
 
+          canvasTime = Math.floor(date.getTime() * Math.PI);
 
-        chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
+          can.setAttribute('id', canvasTime);
 
-          if (msg.action == 'create_image') {
-            if (msg.src != null){
-              console.log(msg.src);
-              ext.createImage(msg.src, sendResponse);
+          body.appendChild(can);
 
-            }
-          }
-        });
 
-        canvasTime = Math.floor(date.getTime() * Math.PI);
-
-        can.setAttribute('id', canvasTime);
-
-        body.appendChild(can);
-
-        document.getElementById(canvasTime).style.display = 'none';
+          document.getElementById(canvasTime).style.display = 'none';
 
     },
 
-    createImage: function(src){
+    checkTop: function(){
+
+      var base,
+          url,
+          video;
+
+      if (window.self !== window.top) {
+
+        video = document.querySelector('video');
+        base = document.querySelector('video').querySelector('source').src;
+        url = base.match(/[^/]+$/);
+
+        video.pause();
+
+        video.addEventListener('canplay', function(){
+
+          ext.createImage(url, base);
+
+        }, false)
+
+        //ext.createImage(url, base);
+
+      }
+
+    },
+
+    msgHandler: function(baseUrl){
+
+      var video,
+          time;
+
+      chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
+
+        video = document.querySelector('video>source[src*="' + msg.src + '"]' ).parentNode;
+        time = video.currentTime;
+
+        console.log(time);
+
+        if (msg.action == 'create_image') {
+          if (msg.src != null){
+
+            if (msg.base !== baseUrl) {
+
+              if (!document.getElementById('iframe-videoCapture-Frame')){
+
+                ext.createIframe(msg.src, msg.full + "#t=" + time);
+
+              }else{
+
+                document.getElementById('iframe-videoCapture-Frame').src = msg.full;
+              }
+
+            } else {
+
+              ext.createImage(msg.src, msg.full);
+
+            }
+
+          }
+        }
+      });
+
+    },
+
+    videoTime: function(){
+
+
+    },
+
+    createImage: function(src, fullUrl){
 
       var vid = document.querySelector('video>source[src*="' + src + '"]' ).parentNode,
           can = document.getElementById(canvasTime),
           con = can.getContext('2d');
-          can.width = vid.offsetWidth;
-          can.height = vid.offsetHeight;
 
-          con.drawImage(vid, 0, 0);
-
-          chrome.extension.sendRequest({image_link: can.toDataURL("img/png")},
-          function(response) {});
-        },
-
-    saveImage: function(){
+          console.log(vid);
+          //vid.addEventListener('loadeddata', function() {
 
 
+      // it's loaded
+
+              can.width = vid.offsetWidth;
+              can.height = vid.offsetHeight;
+
+              con.drawImage(vid, 0, 0, vid.offsetWidth, vid.offsetHeight);
+
+
+
+              chrome.extension.sendRequest({image_link: can.toDataURL("img/png")},
+              function(response) {});
+
+
+           //}, false);
+
+
+    },
+
+    createIframe: function(url, base){
+
+      var vid = document.createElement('iframe');
+
+      var iframeBody;
+      var date = new Date();
+      var time = date.getTime();
+      var frameBody;
+
+        vid.width = 600;
+        vid.height = 500;
+
+        vid.src = base;
+        vid.id = 'iframe-videoCapture-Frame';
+
+        frameBody = document.getElementsByTagName('body')[0].appendChild(vid);
+        document.getElementById('iframe-videoCapture-Frame').style.dispaly = 'none';
+        /*document.getElementById('iframe-videoCapture-Frame').onload = function(){
+          iframeBody = document.getElementById(time).contentWindow.document.querySelector('body');
+          iframeBody.querySelector('video').pause();
+          //ext.createImage(url, base);
+
+        }*/
 
     }
 
-  }
+  };
 
 })();
 
